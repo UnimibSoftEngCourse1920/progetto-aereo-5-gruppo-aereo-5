6dd4473f2cb6862134f8c22cf8639eb1909cf544
samuele-riva <s.riva72@campus.unimib.it>
2020-02-27 13:10:51 +0100

    primo commit

---
+++ Progetto/bookBackEnd/src/main/java/gruppoaereo5/bookBackEnd/App.java
@@ -0,0 +1,13 @@
+package gruppoaereo5.bookBackEnd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
