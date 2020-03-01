4c2e44ba12f67f194ec6497d28b98132d51cf96a
samuele-riva <s.riva72@campus.unimib.it>
2020-03-01 15:51:20 +0100

    Configurazione Hibernate per lavorare con Spring e H2Database

---
+++ Progetto/bookBackEnd/src/main/java/gruppoaereo5/bookBackEnd/config/HibernateConfig.java
@@ -0,0 +1,73 @@
+package gruppoaereo5.bookBackEnd.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan(basePackages= {"gruppoaereo5.bookBackEnd.dto"})
+@EnableTransactionManagement
+public class HibernateConfig {
+	
+	private final static String DATABASE_URL ="jdbc:h2:tcp://localhost/~/bookPlaneTicket";
+	private final static String DATABASE_DRIVER ="org.h2.Driver";
+	private final static String DATABASE_DIALECT ="org.hibernate.dialect.H2Dialect";
+	private final static String DATABASE_USERNAME ="gruppo5";
+	private final static String DATABASE_PASSWORD ="progetto";
+	
+	@Bean
+	public DataSource getDataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		
+		//fornisce informazioni per il collegamento al DB
+		dataSource.setDriverClassName(DATABASE_DRIVER);
+		dataSource.setUrl(DATABASE_URL);
+		dataSource.setUsername(DATABASE_USERNAME);
+		dataSource.setPassword(DATABASE_PASSWORD);
+		
+		return dataSource;
+	}
+	
+	@Bean 
+	public SessionFactory getSessionFactory(DataSource dataSource) {
+		
+		LocalSessionFactoryBuilder builder = new LocalSessionFactoryBuilder(dataSource);
+		
+		builder.addProperties(getHibernateProperties());
+		builder.scanPackages("gruppo5.bookBackEnd.dto");
+		return builder.buildSessionFactory();
+		
+	}
+
+	//metodo che ritorna tutte le HibernateProperties
+	private Properties getHibernateProperties() {
+		
+		Properties properties = new Properties();
+		
+		properties.put("hibernate.dialect", DATABASE_DIALECT);
+		properties.put("hibernate.show_sql", "true");
+		properties.put("hibernate.format_sql", "true");
+		
+		return properties;
+	}
+	
+	@Bean
+	public HibernateTransactionManager getTrasactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
+		return transactionManager;
+	}
+
+	
+	
+	
+
+}
