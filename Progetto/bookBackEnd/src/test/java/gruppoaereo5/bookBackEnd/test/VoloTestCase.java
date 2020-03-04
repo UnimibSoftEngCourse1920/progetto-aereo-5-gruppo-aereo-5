93abfbbffa05101548b020b93c7a1afa936b389e
samuele-riva <s.riva72@campus.unimib.it>
2020-03-04 17:53:59 +0100

    Aggiunta test

---
+++ Progetto/bookBackEnd/src/test/java/gruppoaereo5/bookBackEnd/test/VoloTestCase.java
@@ -0,0 +1,46 @@
+package gruppoaereo5.bookBackEnd.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import gruppoaereo5.bookBackEnd.dao.VoloDAO;
+import gruppoaereo5.bookBackEnd.dto.Volo;
+
+public class VoloTestCase {
+	
+	private static AnnotationConfigApplicationContext context;
+	private static VoloDAO voloDAO;
+	private Volo volo;
+	
+	@BeforeClass
+	public static void init() {
+		context = new AnnotationConfigApplicationContext();
+		context.scan("gruppo5.bookBackEnd");
+		context.refresh();
+		
+		voloDAO = (VoloDAO)context.getBean("VoloDAO");
+	}
+	
+	@Test
+	public void testaddVolo() {
+		volo = new Volo();
+		volo.setCodice_volo("e1");
+		volo.setAereo("fisa");
+		volo.setCitta_partenza("Milano");
+		volo.setCitta_arrivo("Roma");
+		volo.setData_partenza("2010-02-12");
+		volo.setData_arrivo("2010-02-12");
+		volo.setOra_partenza("22:33:00");
+		volo.setOra_arrivo("22:33:00");
+		volo.setKmTratta(122.22);
+		volo.setPrezzo(33.3);
+		volo.setPuntiOttenuti(44);
+		
+		assertEquals("Successo!", true, voloDAO.add(volo));
+	}
+	
+
+}
